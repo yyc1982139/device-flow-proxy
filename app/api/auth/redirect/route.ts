159cbcc589598933876826bb52fc2f57a23236d3
yyc1982139 <yyc1982139@126.com>
2026-04-20 22:43:03 +0800

    Update route.ts

--- app/api/auth/redirect/route.ts
+++ app/api/auth/redirect/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { Cache } from '@/lib/cache';
 import { BASE_URL, TOKEN_ENDPOINT } from '@/lib/config';
 import { StateCacheEntry, DeviceCacheEntry } from '@/lib/cache';
-
+export const dynamic = 'force-dynamic';
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
